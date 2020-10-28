3658a67ff59427d4503687ff6b223a3b5e3021d9
DanilKorabelnikov <71430053+DanilKorabelnikov@users.noreply.github.com>
2020-10-28 19:09:07 +0300

    Update chat.py

--- chat.py
+++ chat.py
@@ -5,7 +5,7 @@ import os
 TOKEN = os.environ.get("TOKEN")
 LINK = os.environ.get("LINK")  # vk.cc/aBAZSc
 SLEEP = os.environ.get("SLEEP")
-api = API(tokens="1d83f56b0ea6d7127e11c9ee01f1024cec0ac720cda5b20bd8444368b747f9d0c268a1a40183d054b244c")
+api = API(tokens=TOKEN)
 
 
 async def post():
@@ -15,14 +15,14 @@ async def post():
             if item == 73482240:
                 await api.wall.post(owner_id=-item, message="Чат для продавцов имущества, аксессуаров и т.д.\n"
                                                                 "Прoдaжa виpтyaльной вaлюты запрещена!\n"
-                                                                "{LINK}\n"
-                                                                "{LINK}")
+                                                                f"{LINK}\n"
+                                                                f"{LINK}")
             else:
                 await api.wall.post(owner_id=-item, message="#arizona #arizonarp\n"
                                                             "Чат для продавцов имущества Arizona RP, аксессуаров и т.д.\n"
                                                             "Прoдaжa виpтyaльной вaлюты запрещена!\n"
-                                                            "{LINK}\n"
-                                                            "{LINK}")
+                                                            f"{LINK}\n"
+                                                            f"{LINK}")
             await asyncio.sleep(60)
         await asyncio.sleep(int(SLEEP))
 
