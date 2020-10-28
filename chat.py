f16a19e6cc071e7d8edf988a686b637f021204b9
DanilKorabelnikov <71430053+DanilKorabelnikov@users.noreply.github.com>
2020-10-28 19:22:10 +0300

    Update chat.py

--- chat.py
+++ chat.py
@@ -5,26 +5,32 @@ import os
 TOKEN = os.environ.get("TOKEN")
 LINK = os.environ.get("LINK")  # vk.cc/aBAZSc
 SLEEP = os.environ.get("SLEEP")
+USER_ID = os.environ.get("USER_ID")
 api = API(tokens=TOKEN)
 
 
 async def post():
     group = [73482240, 112291806]
-    while True:
-        for item in group:
-            if item == 73482240:
-                await api.wall.post(owner_id=-item, message="Чат для продавцов имущества, аксессуаров и т.д.\n"
+    try:
+        while True:
+            for item in group:
+                if item == 73482240:
+                    await api.wall.post(owner_id=-item, message="Чат для продавцов имущества, аксессуаров и т.д.\n"
                                                                 "Прoдaжa виpтyaльной вaлюты запрещена!\n"
                                                                 f"{LINK}\n"
                                                                 f"{LINK}")
-            else:
-                await api.wall.post(owner_id=-item, message="#arizona #arizonarp\n"
-                                                            "Чат для продавцов имущества Arizona RP, аксессуаров и т.д.\n"
-                                                            "Прoдaжa виpтyaльной вaлюты запрещена!\n"
-                                                            f"{LINK}\n"
-                                                            f"{LINK}")
-            await asyncio.sleep(60)
-        await asyncio.sleep(int(SLEEP))
+                else:
+                    await api.wall.post(owner_id=-item, message="#arizona #arizonarp\n"
+                                                                "Чат для продавцов имущества Arizona RP, аксессуаров и т.д.\n"
+                                                                "Прoдaжa виpтyaльной вaлюты запрещена!\n"
+                                                                f"{LINK}\n"
+                                                                f"{LINK}")
+                await asyncio.sleep(60)
+            await asyncio.sleep(int(SLEEP))
+    except:
+        await api.messages.send(user_id=int(USER_ID), random_id=api.extension.random_id(), peer_id=int(USER_ID),
+                                message="Произошла ошибка при создание поста!")
+      
 
 
 if __name__ == '__main__':
