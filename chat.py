1f3800dc0384389f1e7e47fccb5369d8305e90ef
DanilKorabelnikov <71430053+DanilKorabelnikov@users.noreply.github.com>
2020-10-28 18:49:05 +0300

    Update chat.py

--- chat.py
+++ chat.py
@@ -5,27 +5,19 @@ import os
 TOKEN = os.environ.get("TOKEN")
 LINK = os.environ.get("LINK")  # vk.cc/aBAZSc
 SLEEP = os.environ.get("SLEEP")
-LIST = [-73482240, -42590964, -112291806]
+OWNER_ID = os.environ.get("OWNER_ID")
 api = API(tokens=TOKEN)
 
 
 async def post():
     while True:
-        for i in LIST:
-            if i != -73482240:
-                await api.wall.post(owner_id=i, message="#arizonarp\n"
-                                                        "Чат для продавцов имущества Arizona RP, аксессуаров и т.д.\n"
-                                                        "Продажа виртуальной валюты запрещена!"
-                                                        f"{LINK}\n"
-                                                        f"{LINK}")
-            else:
-                await api.wall.post(owner_id=i, message="Чат для продавцов имущества, аксессуаров и т.д.\n"
-                                                        "Продажа виртуальной валюты запрещена!"
-                                                        f"{LINK}\n"
-                                                        f"{LINK}")
-            await asyncio.sleep(60)
+        await api.wall.post(owner_id=OWNER_ID, message="Чат для продавцов имущества, аксессуаров и т.д.\n"
+                                                "Продажа виртуальной валюты запрещена!"
+                                                f"{LINK}\n"
+                                                f"{LINK}")
         await asyncio.sleep(int(SLEEP))
 
 
 if __name__ == '__main__':
-    asyncio.run(post())
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(post())
