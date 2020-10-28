987b21cedaae94749ec241d0753ebf82fff089b8
DanilKorabelnikov <71430053+DanilKorabelnikov@users.noreply.github.com>
2020-10-28 14:06:13 +0300

    Add files via upload

---
+++ chat.py
@@ -0,0 +1,22 @@
+from vkbottle.api import API
+import asyncio
+import os
+
+TOKEN = os.environ.get("TOKEN")
+OWNER_ID = os.environ.get("OWNER_ID")
+LINK = os.environ.get("LINK")  # vk.cc/aBAZSc
+SLEEP = os.environ.get("SLEEP")
+api = API(tokens=TOKEN)
+
+
+async def post():
+    while True:
+        await api.wall.post(owner_id=OWNER_ID, message="Чат для продавцов имущества, аксесуаров и т.д.\n"
+                                                       "Продажа виртуальной валюты запрещена!"
+                                                       f"{LINK}\n"
+                                                       f"{LINK}")
+        await asyncio.sleep(int(SLEEP))
+
+
+if __name__ == '__main__':
+    asyncio.run(post())
